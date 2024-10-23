2d732ecdc80e5adefa75f31a931b8a31fc437a8d
murilott <murilo.teichert@univille.br>
2024-10-23 19:55:19 -0300

    appcontainer e desktop container apresentam sinais de funcionamento

--- main.py
+++ main.py
@@ -24,10 +24,11 @@ class Application:
         # self.frame.pack(side="top")
 
         self.desktopContainer = Frame(master, borderwidth = 1, bg="gray")
-        self.desktopContainer["pady"] = 10
-        self.desktopContainer["width"] = 200
+        self.desktopContainer["pady"] = 20
+        self.desktopContainer["width"] = 300
         self.desktopContainer["height"] = 100
         self.desktopContainer.pack(fill="both", expand=False, side="top")
+        # self.desktopContainer.pack_propagate(False)
 
         self.appListContainer = Frame(master, borderwidth = 1, bg="lightgray")
         self.appListContainer["padx"] = 20
@@ -38,33 +39,34 @@ class Application:
 
         self.appContainer = Frame(self.desktopContainer, borderwidth = 1, bg="lightgray")
         self.appContainer["pady"] = 5
-        self.appContainer["width"] = 50
-        self.appContainer["height"] = 50
+        self.appContainer["width"] = 500
+        self.appContainer["height"] = 200
+        self.appContainer.pack_propagate(False)
 
         self.botApp1 = Button(self.appListContainer, text="App 1", command=self.abrir_app) #self.desktopContainer, 1
         self.botApp1["width"] = 5
         self.botApp1.pack(side="left")
 
-        self.botApp2 = Button(self.appListContainer, text="App 2", command=self.minimizar)
+        self.botApp2 = Button(self.appListContainer, text="App 2")
         self.botApp2["width"] = 5
         self.botApp2.pack(side="left")
 
-        self.botApp3 = Button(self.appListContainer, text="App 3", command=self.minimizar)
+        self.botApp3 = Button(self.appListContainer, text="App 3")
         self.botApp3["width"] = 5
         self.botApp3.pack(side="left")
 
-        self.botMinimizar = Button(self.desktopContainer, text="Minimizar", command=self.minimizar)
-        self.botMinimizar["width"] = 10
-        self.botMinimizar.pack(side="top")
+        self.botApagar = Button(self.appContainer, text="Fechar", command=self.fechar)
+        self.botApagar["width"] = 8
+        self.botApagar["height"] = 1
+        self.botApagar.pack(side="right", anchor="ne")
+        
+        self.botMinimizar = Button(self.appContainer, text="Minimizar", command=self.abrir_app)
+        self.botMinimizar["width"] = 8
+        self.botMinimizar["height"] = 1
+        self.botMinimizar.pack(side="right", anchor="ne")
 
-        self.botApagar = Button(self.desktopContainer, text="Fechar", command=self.fechar)
-        self.botApagar.pack(side="top")
 
     def abrir_app(self): #, root, app
-        print("abrir app")
-        
-        # self.appContainer.pack(expand=False, side="bottom")
-
         if self.appContainer.winfo_manager():
             self.appContainer.pack_forget()
         else:
