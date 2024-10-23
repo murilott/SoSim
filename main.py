e413da0a0ec21df741f963aa087a0b45b3fbdeea
murilott <murilo_teichert@hotmail.com>
2024-10-23 17:25:20 -0300

    testar usar bind para passar parâmetros

--- main.py
+++ main.py
@@ -29,6 +29,30 @@ class Application:
         self.desktopContainer["height"] = 100
         self.desktopContainer.pack(fill="both", expand=False, side="top")
 
+        self.appListContainer = Frame(master, borderwidth = 1, bg="lightgray")
+        self.appListContainer["padx"] = 20
+        self.appListContainer["pady"] = 10
+        self.appListContainer["width"] = 250
+        self.appListContainer["height"] = 100
+        self.appListContainer.pack(expand=False, side="top")
+
+        self.appContainer = Frame(self.desktopContainer, borderwidth = 1, bg="lightgray")
+        self.appContainer["pady"] = 5
+        self.appContainer["width"] = 50
+        self.appContainer["height"] = 50
+
+        self.botApp1 = Button(self.appListContainer, text="App 1", command=self.abrir_app) #self.desktopContainer, 1
+        self.botApp1["width"] = 5
+        self.botApp1.pack(side="left")
+
+        self.botApp2 = Button(self.appListContainer, text="App 2", command=self.minimizar)
+        self.botApp2["width"] = 5
+        self.botApp2.pack(side="left")
+
+        self.botApp3 = Button(self.appListContainer, text="App 3", command=self.minimizar)
+        self.botApp3["width"] = 5
+        self.botApp3.pack(side="left")
+
         self.botMinimizar = Button(self.desktopContainer, text="Minimizar", command=self.minimizar)
         self.botMinimizar["width"] = 10
         self.botMinimizar.pack(side="top")
@@ -36,12 +60,23 @@ class Application:
         self.botApagar = Button(self.desktopContainer, text="Fechar", command=self.fechar)
         self.botApagar.pack(side="top")
 
+    def abrir_app(self): #, root, app
+        print("abrir app")
+        
+        # self.appContainer.pack(expand=False, side="bottom")
+
+        if self.appContainer.winfo_manager():
+            self.appContainer.pack_forget()
+        else:
+            self.appContainer.pack(expand=False, side="bottom")
+
     def fechar(self):
         pass
 
-
-
     def minimizar(self): #, event
+        self.desktopContainer.pack_forget() if self.desktopContainer.winfo_manager() else self.desktopContainer.pack(anchor=W, padx=5, pady=10)
+
+    def minimizar2(self): #, event
         if self.msg["text"] == "Primeiro widget":
             self.msg["text"] = "O botão recebeu um clique"
         else:
